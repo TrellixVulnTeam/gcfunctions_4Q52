f24b33c600d24688e39f6d84b480775b2a35d297
Bill Hahn <BillHahnG@gmail.com>
2017-04-30 02:38:51 -0500

    Change Greeting to Hey Willy

--- helloworld/index.js
+++ helloworld/index.js
@@ -1,5 +1,5 @@
 exports.helloworld = function helloworld(req, res) {
-  let name = req.query.name || 'You';
+  let name = req.query.name || 'Willy';
   let message = 'Hey ' + name;
   console.log(message);
   res.status(200).send('Success: ' + message);
