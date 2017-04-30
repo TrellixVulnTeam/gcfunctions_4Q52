ac9ff1a05c98e7c1ccf4eb9792c6cb90e7205086
Bill Hahn <BillHahnG@gmail.com>
2017-04-30 15:24:27 -0500

    Bill Change.

--- helloworld/index.js
+++ helloworld/index.js
@@ -1,5 +1,5 @@
 exports.helloworld = function helloworld(req, res) {
-  let name = req.query.name || 'William';
+  let name = req.query.name || 'Bill';
   let message = 'Hey ' + name;
   console.log(message);
   res.status(200).send('Success: ' + message);
