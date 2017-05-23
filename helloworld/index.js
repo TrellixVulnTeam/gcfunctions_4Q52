4c8a6a153efa491cc977c2438fe92338d7cdae27
Bill Hahn <BillHahnG@gmail.com>
2017-05-23 03:49:16 -0500

    Datastore Logic Added

--- helloworld/index.js
+++ helloworld/index.js
@@ -1,6 +1,32 @@
-exports.helloworld = function helloworld(req, res) {
+/*exports.helloworld = function helloworld(req, res) {
   let name = req.query.name || 'Bill';
   let message = 'Hey ' + name;
   console.log(message);
   res.status(200).send('Success: ' + message);
+};
+*/
+
+exports.helloworld = function helloworld(req, res) {
+  let name = req.query.name || 'Apple';
+  let message = 'Hey you should type a product name ';
+
+  // [START prodouctQuery]
+  function searchProduct (name) {
+    const query = datastore.createQuery('Product')
+    .filter('name', ">=", name)
+    .filter('name', "<", name+"~")
+    .select('name')
+    .order('name', {
+      descending: true
+    });
+
+  datastore.runQuery(query)
+    .then((results) => {
+      // Product entities found.
+      const Products = results[0];
+      console.log('Product Autocomplete: ');
+      Products.forEach((name) => console.log(name));
+    });
+  Products.forEach((name) => res.status(200).send(name));
+  }
 };
